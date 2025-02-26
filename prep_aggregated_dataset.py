ccc710a10b6134f4a02fc751f88fba0d73e4f851
taniki <tk@renard.ninja>
2025-02-26 17:50:43 +0100

    add a column with unified money amount

--- prep_aggregated_dataset.py
+++ prep_aggregated_dataset.py
@@ -36,8 +36,75 @@ def _():
 
 
 @app.cell
-def _():
+def _(mo):
+    mo.md(r"""## harmonise les montants payés en euros""")
     return
+
+
+@app.cell
+def _():
+    import currency_converter
+    return (currency_converter,)
+
+
+@app.cell
+def _(currency_converter):
+    c = currency_converter.CurrencyConverter(fallback_on_missing_rate=True)
+    return (c,)
+
+
+@app.cell
+def _(c, df, pd):
+    Spend_Euros = (
+        df
+        .assign(
+            StartDate = lambda df: pd.to_datetime(df.StartDate)
+        )
+        .pipe(lambda df_: df_[df_.StartDate <= "2025-02-12"])
+        .assign(
+            StartDate = lambda df: df.StartDate.dt.date
+        )
+        .apply(
+            lambda r: (
+                c.convert(
+                    amount=r["Spend"],
+                    currency=r["Currency Code"],
+                    date=r["StartDate"]
+                )
+            ), axis=1
+        )
+        .astype('int')
+    )
+    return (Spend_Euros,)
+
+
+@app.cell
+def _(Spend_Euros, df, pd):
+    df_final = (
+        df
+        .assign(
+            StartDate = lambda df: pd.to_datetime(df.StartDate)
+        )
+        .pipe(lambda df_: df_[df_.StartDate <= "2025-02-12"])
+        .assign(
+            Spend_Euros = Spend_Euros
+        )
+    )
+
+    df_final
+    return (df_final,)
+
+
+@app.cell
+def _(df_final):
+    df_final.to_csv('datasets/politicalads-2018-20250212-alleuros.csv')
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
 
 
 if __name__ == "__main__":
