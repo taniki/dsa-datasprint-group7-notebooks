d67533781fb600bc17e4562fb7196d4d3d9a7828
taniki <tk@renard.ninja>
2025-02-27 23:22:24 +0100

    add a boxplot visualisation

--- cook_countries_univariates.py
+++ cook_countries_univariates.py
@@ -11,33 +11,17 @@ def _(mo):
 
 
 @app.cell(hide_code=True)
-def _(df, metrics_radio, mo):
-    table = (
-        df
-        .groupby('CountryCode')
-        [ metrics_radio.value ]
-        .agg(['count', 'sum', 'mean', 'std', 'median'])
-        .sort_values('mean', ascending=False)
-    )
-
-    def format_float(x):
-        return '{:.2f}'.format(x) if metrics_radio.value != 'cost_per_impression' else '{:.4f}'.format(x)
-
-    table_ui = mo.ui.table(
-        table,
-        selection=None,
-        format_mapping={
-            'mean': format_float,
-            'std': format_float,
-            'median': format_float,
-        }
-    )
-
+def _(df, metrics_radio, mo, plot_static, show_outliers, table_ui):
     mo.hstack([
         metrics_radio,
-        table_ui
+        mo.vstack([
+            table_ui,
+            plot_static(df.query('CountryCode.isin(@table_ui.value.index.to_list())')),
+            show_outliers
+            #plot(df.query('CountryCode.isin(@table_ui.value.index.to_list())'))
+        ])
     ])
-    return format_float, table, table_ui
+    return
 
 
 @app.cell
@@ -90,6 +74,83 @@ def _(df):
 
 @app.cell
 def _(mo):
+    mo.md("""## graphics""")
+    return
+
+
+@app.cell
+def _():
+    from pyobsplot import Plot
+    return (Plot,)
+
+
+@app.cell
+def _(metrics_radio, plt, show_outliers):
+    def plot_static(df):
+        fig, ax = plt.subplots(figsize=(15,3))
+
+        if (len(df) > 0):
+            (
+                df
+                .plot
+                .box(
+                    column=metrics_radio.value,
+                    by="CountryCode",
+                    showfliers=show_outliers.value,
+                    ax=ax
+                )
+            )
+        #plt.show()
+
+        return fig
+    return (plot_static,)
+
+
+@app.cell
+def _(df, plot_static):
+    plot_static(df.query('CountryCode.isin(@table_ui.value.index.to_list())'))
+    return
+
+
+@app.cell
+def _():
+    import matplotlib.pyplot as plt
+    return (plt,)
+
+
+@app.cell
+def _(Plot, metrics_radio):
+    def plot(data):    
+        return Plot.plot({
+            "marginLeft": 100,
+            "y": {"grid": True},
+            "x": {
+                "label": None,
+            },
+            # color: {legend: true},
+            "marks": [
+                Plot.dot(data, Plot.dodgeX("middle", {"fx": "CountryCode", "y": metrics_radio.value}))
+            ]
+        })
+
+    #plot(df.query('CountryCode.isin(@table_ui.value.index.to_list())'))
+    return (plot,)
+
+
+@app.cell
+def _(table_ui):
+    table_ui.value.index.to_list()
+    return
+
+
+@app.cell
+def _(df):
+    df.query('CountryCode.isin(@table_ui.value.index.to_list())')
+    return
+
+
+@app.cell
+def _(mo):
     mo.md("""## UI helpers""")
     return
 
@@ -113,6 +174,44 @@ def _(metrics, mo):
 @app.cell
 def _(metrics_radio):
     metrics_radio
+    return
+
+
+@app.cell
+def _(df, metrics_radio, mo):
+    table = (
+        df
+        .groupby('CountryCode')
+        [ metrics_radio.value ]
+        .agg(['count', 'sum', 'mean', 'std', 'median'])
+        .sort_values('mean', ascending=False)
+    )
+
+    def format_float(x):
+        return '{:.2f}'.format(x) if metrics_radio.value != 'cost_per_impression' else '{:.4f}'.format(x)
+
+    table_ui = mo.ui.table(
+        table,
+        #selection=None,
+        format_mapping={
+            'mean': format_float,
+            'std': format_float,
+            'median': format_float,
+        }
+    )
+
+    return format_float, table, table_ui
+
+
+@app.cell
+def _(mo):
+    show_outliers = mo.ui.checkbox(label="display outliers")
+    return (show_outliers,)
+
+
+@app.cell
+def _(show_outliers):
+    (show_outliers, show_outliers.value)
     return
 
 
